d3e287a01f03a6626527159caef32473ac9bccbb
nadcdc <nadcdc4@gmail.com>
2018-11-05 13:20:54 +0900

    dockerhub에 올라간 base이미지와 deploy.sh에서 requriement저장하는부분 중복되서 deploy.sh에서 삭제함.

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 
-# requirements만들기
-pipenv lock --requirements > requirements.txt
-
-# .secrets와 requriements를 starging area에 추가.
-git add -f .secrets/ requirements.txt
+# .secrets를 starging area에 추가.
+git add -f .secrets/
 
 # eb deploy 실행.
 eb deploy --profile fc-8th-eb-second --staged
 
-# .secrets와 requirements를 staging area에서 제거
-git reset HEAD requirements.txt, .secrets
+# .secrets를 staging area에서 제거
+git reset HEAD  .secrets/
 
-# requriements.txt 삭제
-rm -f requirements.txt
