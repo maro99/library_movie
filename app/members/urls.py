0cb18bd51b0b6929003f1333c0b0388f57d63f0a
nadcdc <nadcdc4@gmail.com>
2018-11-09 11:19:03 +0900

    카카오 로그인 코드 login page에서 어떤 로긴인지 상태 보여주도록함. + id보여줌

--- app/members/urls.py
+++ app/members/urls.py
@@ -5,6 +5,7 @@ from .views import *
 urlpatterns = [
 
     path('login_page/', login_page, name='login_page'),
-    path('login/', normal_login, name='login'),
-    path('facebook_login/', facebook_login, name='facebook_login')
+    path('normal_login/', normal_login, name='normal_login'),
+    path('facebook_login/', facebook_login, name='facebook_login'),
+    path('kakaotalk_login/', kakaotalk_login, name='kakaotalk_login')
 ]
--- app/members/views.py
+++ app/members/views.py
@@ -1,7 +1,9 @@
+import inspect
 import sys
 
 import django
 from django.contrib.auth import get_user_model, login
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 import requests
 
@@ -15,7 +17,7 @@ def normal_login(request):
 
 
     if request.method == 'POST':
-        return render(request, 'main.html')
+        return render(request, 'normal_login_succed.html')
 
     else:
         return render(request,'members/login.html')
@@ -59,7 +61,7 @@ def facebook_login(request):
             FACEBOOK_APP_ID, FACEBOOK_APP_SECRET_CODE
         )
     }
-    # response = requests.get(url, params)
+    response = requests.get(url, params)
 
     # 위에서 받은 response중 이름등 추가적 가져오기 위해서 scope를 전달해 줘야 한다. ----> http에 추가해 놓았다.
 
@@ -103,19 +105,113 @@ def facebook_login(request):
 
     if user is not None:
         login(request, user, backend='django.contrib.auth.backends.ModelBackend')
-        return render(request,'facebook_login_succed.html')
+
+        # 어떤 함수 테스트 중인지도 전달해 주겠다.
+        frame = inspect.currentframe()
+        function_name = inspect.getframeinfo(frame).function
+        context = {'function_name': function_name}
+
+        return render(request,'social_login_succed.html',context)
+
+    return redirect('login_page')
+
+def kakaotalk_login(request):
+
+    # 1. code 받기
+    code = request.GET.get('code')
+    # return HttpResponse(code)
+
+
+    # 2 .access token 받기
+    url = "https://kauth.kakao.com/oauth/token"
+    kakaotalk_redirect_uri = 'https://maro5.com/members/kakaotalk_login/'
+
+    RUNSERVER = 'runserver' in sys.argv
+    if RUNSERVER:
+        kakaotalk_redirect_uri = 'http://localhost:8000/members/kakaotalk_login/'
+
+    params = {
+        'grant_type':'authorization_code',
+        'client_id':KAKAOTALK_REST_API_KEY,
+        'redirect_uri':kakaotalk_redirect_uri,
+        'code':code
+    }
+
+    response = requests.post(url,params)
+    response_dict = response.json()
+    access_token = response_dict['access_token']
+    # return HttpResponse(access_token)
+
+
+    # 3. access token 이용해서 app loigin
+    # 엡연결-로긴 은 로그인한 사용자와 앱을 카카오 플랫폼에 연결함으로서
+    # 일반적인 사용자가 앱 가입/등록 요청을 하는 경우와 비슷하다.
+    # 카카오 서비스 이용하기 위해서 로그인 후 앱연결 선행되야 하고 앱연결 올바로 수행되면 사용자대한 고유한 아이디 부여된다.
+    url = "https://kapi.kakao.com/v1/user/signup"
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Cache-Control': 'no-cache',
+    }
+    headers.update({'Authorization':'Bearer ' + str(access_token)})
+    response = requests.post(url, headers=headers)
+    # return HttpResponse(response)
+
+
+    # 4. acces _token 이용해서 사용자 정보 요청.
+    # 사용자의 id, 카카오 계정 email 및 상세정보 얻어올 수 있는 기능.
+    # 사용자 로그인 후 얻은 사용자토큰, 엡연결이 되어있어야 한다.
+    url = "https://kapi.kakao.com/v1/user/me"
+    response = requests.post(url, headers=headers)
+    # return HttpResponse(response)
+
+    response_dict = response.json()
+
+    #5. 받아온 정보 중 회원가입에 필요한 요소들 꺼내기 및 회원 가입
+    id = response_dict['id']
+    email = response_dict['kaccount_email']
+    nickname = response_dict['properties']['nickname']
+    url_img_profile = response_dict['properties']['profile_image']#['url']
+
+    # 있으면 get없으면 create하고 True도 같이 반환 .
+    user, user_created = User.objects.get_or_create(
+        username = id,
+        email = email,
+    )
+
+    login(request, user,backend='django.contrib.auth.backends.ModelBackend')
+
+    # return redirect('index')
+
+    if user is not None:
+        login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+
+        # 어떤 함수 테스트 중인지도 전달해 주겠다.
+        frame = inspect.currentframe()
+        function_name = inspect.getframeinfo(frame).function
+        context = {'function_name': function_name}
+
+        return render(request,'social_login_succed.html',context)
 
     return redirect('login_page')
 
 
+
 def login_page(request):
 
-    redirect_uri = 'https://maro5.com/members/facebook_login/'
+    facebook_redirect_uri = 'https://maro5.com/members/facebook_login/'
+    kakaotalk_redirect_uri = 'https://maro5.com/members/kakaotalk_login/'
+
+    # 런서버 중이면 로컬 호스트로 redirect 시케겠다.
     RUNSERVER = 'runserver' in sys.argv
     if RUNSERVER:
-        redirect_uri = 'http://localhost:8000/members/facebook_login/'
+        facebook_redirect_uri = 'http://localhost:8000/members/facebook_login/'
+        kakaotalk_redirect_uri = 'http://localhost:8000/members/kakaotalk_login/'
 
-    context = {"redirect_uri":redirect_uri}
+
+    context = {"facebook_redirect_uri":facebook_redirect_uri,
+               "kakaotalk_redirect_uri":kakaotalk_redirect_uri,
+               "kakaotalk_rest_api_key": KAKAOTALK_REST_API_KEY,}
+
 
 
     return render(request, 'members/login.html',context =context)
