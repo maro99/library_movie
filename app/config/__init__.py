4b0184cf542251c342b29a9f7032070d660863f8
nadcdc <nadcdc4@gmail.com>
2018-12-05 19:58:45 +0900

    이 상태에서 local에서 redis-cerlery 이용, email_send.delay()해봄.
    runserver,
    celery -A config worker --loglevel=info,
    redis-server
    
    email로 아이디 api-view로 postman통해 만드는 요청 보내면
    celery-worker창에 django -> redis로 부터 받은 tasks 보이고
    redis-cli -h 127.0.0.1 -p 6379 해서
    redis에 tasks 쌓였던것 볼 수 있음

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,21 +1,12 @@
-# from __future__ import absolute_import, unicode_literals
-#
-# # This will make sure the app is always imported when
-# # Django starts so that shared_task will use this app.
-#
-# # 이하dml  이 app이 장고가 실행될때 항상 import되도록 한다.
-# # @shared_task 데코레이터가 이엡을 이용할 것이다.
-#
-# from .celery_setting import  app as celery_app
-#
-# __all__=('celery_app')
+from __future__ import absolute_import, unicode_literals
 
-#위쪽은 우리 프로젝트에서 celelry +rabbitmq 하던것
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
 
+# 이하dml  이 app이 장고가 실행될때 항상 import되도록 한다.
+# @shared_task 데코레이터가 이엡을 이용할 것이다.
 
+from .celery_setting import  app as celery_app
 
-#이하는 이번에 celelry+ redis하려고 경영학도 따라하는중.
+__all__=('celery_app')
 
-from .tasks import app as celery_app
-
-__all__ = ['celery_app']
