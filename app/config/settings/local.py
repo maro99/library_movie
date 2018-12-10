c1f1126366531af92d193c9306e992893f84543d
nadcdc <nadcdc4@gmail.com>
2018-12-10 23:15:20 +0900

    redis+ celeryd +celerybeat -> daemon docker에 넣고 해보기 (  dev모드에서 docker안에서  이 셋을 supervisor로 실행시키기) 진행중
    step1) )셀러리 -> 확인

--- app/config/settings/local.py
+++ app/config/settings/local.py
@@ -23,6 +23,8 @@ DATABASES = {
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
+
+
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TAST_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
