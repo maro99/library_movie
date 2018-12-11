c34f474aee9f063411d62cb913134372ce0fb0e7
nadcdc <nadcdc4@gmail.com>
2018-12-12 00:09:25 +0900

    redis+celery 배포 환경에서 도는거 확인해봄 (elasticache x , dockercompose x)

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -22,6 +22,22 @@ INSTALLED_APPS += [
 # DB
 DATABASES = secrets['DATABASES']
 
+
+# CELERY + Redis
+# CELERY_BROKER_URL = 'redis://localhost:6379/0'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+CELERY_BROKER_URL = 'redis://0'
+CELERY_RESULT_BACKEND = 'redis://0'
+
+# CELERY_BEAT
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TAST_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Seoul' #Celery beat가 스케줄러이기 때문에 시간에 대한 정의를 해야함
+
+
+
 # Media
 DEFAULT_FILE_STORAGE = "config.storages.S3DefaultStorage"
 AWS_STORAGE_BUCKET_NAME = secrets["AWS_STORAGE_BUCKET_NAME"]
