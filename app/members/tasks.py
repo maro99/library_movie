083a783e6a49cafabfc4dbbfd6672e72520618b9
nadcdc <nadcdc4@gmail.com>
2018-12-16 23:56:33 +0900

    회원가입 링크 보내는 template을 email_send시에 no_reverse_match뜨면서 안되던 에러 해결 + tasks.py, email보내는 html에서 런서버 여부 판단해서 도메인 정해서 보내도록 함

--- app/members/tasks.py
+++ app/members/tasks.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import, unicode_literals
 import random
 
 # Celery 사용 위한것
+import sys
+
 from config import celery_app
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
@@ -18,12 +20,23 @@ User = get_user_model()
 @celery_app.task
 def send_email(pk):
 
+
+    RUNSERVER = '/home/nasanmaro/.local/share/virtualenvs/library_movie-hgMtabiI/bin/celery' in sys.argv
+    if RUNSERVER:
+        domain = "localhost:8000"
+    else:
+        domain = "maro5.com"
+
+    print(sys.argv)
+    print('hello world')
+
+
     user = User.objects.get(pk=pk)
 
     # current_site = get_current_site(self.context['request']
     message = render_to_string('members/account_activate_email.html', {
         'user': user,  # 생성한 사용자 객체
-        'domain': 'maro5.com', #localhost:8000',  # 이거 추후에 배포시에는 바꿔줬던것 같다? #########
+        'domain': domain, #localhost:8000', "maro5.com"  # 이거 추후에 배포시에는 바꿔줘야함 #########
         'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode('utf-8'),  # 생성한 사용자 객체의 pk를 암호화한 값
         'token': account_activation_token.make_token(user)  # 생성한 사용자 객체를 통해 생성한 token값.
     })
