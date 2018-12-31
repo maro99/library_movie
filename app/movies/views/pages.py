68dbc7add44cfddad128dc96050835a993862ea4
nadcdc <nadcdc4@gmail.com>
2018-12-31 22:32:44 +0900

    main_page뷰에서 오늘날자 이후 것들 보여주지 않도록

--- app/movies/views/pages.py
+++ app/movies/views/pages.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 
 from movies.models import Movie
+import datetime
+from django.utils import timezone
 
 
 def main_page(request):
 
-    movies=Movie.objects.all()
+    movies = Movie.objects.order_by("when")
+
+
+    movies_filtered = movies
+
+    # 날짜 지난 친구들은 안보여주겠다면 주석 풀어라
+    # movies_filtered = list()
+    # now = timezone.now()
+    # for movie in movies:
+    #     if movie.when >= now:
+    #         movies_filtered.append(movie)
+
+
+
 
     context = {
-        "movies":movies
+        "movies":movies_filtered
     }
     return render(request, 'movie/main.html',context=context)
 
