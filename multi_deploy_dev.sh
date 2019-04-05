f16e194bede44c83f7aa78b63cbacca1dd61f67c
nadcdc <nadcdc4@gmail.com>
2019-04-06 02:29:38 +0900

    cat명령어 사용해서 root_address 업데이트 위해 front쪽 js파일 하나 더 만들고 docker dev multi 배포 스크립트에서 cat 으로 js 파일 수정

--- multi_deploy_dev.sh
+++ multi_deploy_dev.sh
@@ -3,6 +3,9 @@
 # 프론트 프로젝트 로컬로 클론 해옴.
 git clone git@github.com:maro99/library_movie_frontend.git front
 
+# root_url dev환경 맞게 변경
+cat front/js/address_variable_dev.js > front/js/address_variable.js
+
 # 도커 빌드
 ./build.py -m dev
 
