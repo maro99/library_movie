ab102180090afd0ff6d10c3077c8e61102bf12ab
nadcdc <nadcdc4@gmail.com>
2019-05-06 19:53:24 +0900

    production 환경에서 load balancer healthcheck 위한 allowed host추가 코드 작성

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -107,3 +107,36 @@ LOGGING = {
         }
     }
 }
+
+
+# Elastic load Balance HealthCheck 4xx에러 대응 해서 ec2의 private ip 추가해줌.
+def is_ec2_linux():
+    """Detect if we are running on an EC2 Linux Instance
+       See http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/identify_ec2_instances.html
+    """
+    if os.path.isfile("/sys/hypervisor/uuid"):
+        with open("/sys/hypervisor/uuid") as f:
+            uuid = f.read()
+            return uuid.startswith("ec2")
+    return False
+
+
+def get_linux_ec2_private_ip():
+    """Get the private IP Address of the machine if running on an EC2 linux server"""
+    from urllib.request import urlopen
+    if not is_ec2_linux():
+        return None
+    try:
+        response = urlopen('http://169.254.169.254/latest/meta-data/local-ipv4')
+        ec2_ip = response.read().decode('utf-8')
+        if response:
+            response.close()
+        return ec2_ip
+    except Exception as e:
+        print(e)
+        return None
+
+
+private_ip = get_linux_ec2_private_ip()
+if private_ip:
+    ALLOWED_HOSTS.append(private_ip)
