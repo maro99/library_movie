22352098adff3f66fec16cc93f12ad798bbd3e6d
nadcdc <nadcdc4@gmail.com>
2019-06-04 17:43:48 +0900

    크롤링 시간 변경

--- app/config/celery_setting.py
+++ app/config/celery_setting.py
@@ -61,7 +61,7 @@ app.conf.beat_schedule = {
     },
     'add-every-minute-contrab': {
         'task': 'crawling_then_send_result_email',
-        'schedule': crontab(minute=10, hour=2),
+        'schedule': crontab(minute=46, hour=17),
         'args': (),
     },
 }
